6ee705f63c29dfe8803fe6b62723f5b93a292c90
Rocio <ropiotti@gmail.com>
2020-10-28 13:26:03 +0100

    Setup failure tests

--- src/components/GuessedWords/GuessedWords.test.js
+++ src/components/GuessedWords/GuessedWords.test.js
@@ -3,10 +3,9 @@ import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../../test/testUtils";
 import GuessedWords from "./GuessedWords";
 
-const defaultProps =  {
-    guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
 };
-
 
 const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
@@ -16,3 +15,20 @@ const setup = (props = {}) => {
 test("does not a warning with expectec props", () => {
   checkProps(GuessedWords, defaultProps);
 });
+
+describe("If there are no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test("renderes without error", () => {
+    const component = findByTestAttr(wrapper, "component-guest-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders instraction to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instruction");
+    expect(instructions.text().length).not.toBe(0)
+  });
+});
+
+describe("If there are words guessed", () => {});
