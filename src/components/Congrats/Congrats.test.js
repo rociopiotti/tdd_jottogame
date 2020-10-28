ec6cc96dd2655fe3708070bad590f366e3d67744
Rocio <ropiotti@gmail.com>
2020-10-28 10:29:41 +0100

    Add first test: renders without errors

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -2,16 +2,20 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Congrats from "./Congrats";
-
+import { findByTestAttr } from "../../test/testUtils";
 
 // set up for enzyme adapter ( not official for react version 17)
 Enzyme.configure({ adapter: new Adapter() });
 
 const setup = (props = {}) => {
-    return shallow(<Congrats {...props}/>)
+  return shallow(<Congrats {...props} />);
 };
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component).toBe(1);
+});
 
 test('renders no text when "succes prop is false"', () => {});
 
